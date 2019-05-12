247ecef3db3d6195b01db5fc78f979ac2250e413
Oliver Gordon <oliver.gordon@nottingham.ac.uk>
2019-05-12 22:14:11 +0100

    Save tokenizer to allow for prediction in future

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
+import pickle
 
 # Read in data
 data = pd.read_csv("data.csv")
@@ -16,12 +17,14 @@ num_words = 50000
 tensorboard = TensorBoard(log_dir='logs/')
 checkpoints = ModelCheckpoint('model_checkpoint.h5',
                               monitor='val_loss', mode='min', save_best_only=True)
-earlystopping = EarlyStopping(monitor='val_loss', patience=50)
+earlystopping = EarlyStopping(monitor='val_loss', patience=10)
 
 binarizer = MultiLabelBinarizer()
 tokenizer = Tokenizer(num_words=num_words, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
 tokenizer.fit_on_texts(data.headline.values)
 word_index = tokenizer.word_index
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 # Generate dataset
 x = tokenizer.texts_to_sequences(data.capitalised_index.values)
@@ -39,6 +42,6 @@ model.add(Bidirectional(LSTM(100, dropout=0.2, recurrent_dropout=0.2)))
 model.add(Dense(y.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=150, batch_size=512, verbose=1, validation_data=(x_test, y_test),
+model.fit(x_train, y_train, epochs=150, batch_size=2048, verbose=1, validation_data=(x_test, y_test),
           callbacks=[tensorboard, earlystopping, checkpoints])
 scores = model.evaluate(x_test, y_test, verbose=1)
