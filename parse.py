aaf40f6765d06dfe4be37564e8519d04cad32fd0
Oliver Gordon <oliver.gordon@nottingham.ac.uk>
2019-05-14 10:54:50 +0100

    include daily star

--- parse.py
+++ parse.py
@@ -1,31 +1,47 @@
+import os
+from itertools import product
+
 import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
-import os
 
-database_name = "data_no_short_words.csv"
+database_name = "data_no_short_words_new.csv"
+paper_names = ["daily star", "daily mail"]
+
 lowercase_text = []
 one_hot = []
 pub_date = []
+paper = []
 
 if os.path.isfile(database_name):
-    start_date = pd.read_csv(database_name).tail(1).pub_date.values[0] + pd.Timedelta(days=1)
+    start_date = pd.to_datetime(pd.read_csv(database_name).tail(1).pub_date.values[0]) + pd.Timedelta(days=1)
 else:
     start_date = '1996-01-01'
 end_date = pd.datetime.today()
+date_range = pd.date_range(start_date, end_date)
 
 print("Reading Archive Webpages:")
-for date in tqdm(pd.date_range(start_date, end_date)):
-    url = "https://www.dailymail.co.uk/home/sitemaparchive/day_%04d%02d%02d.html" % (date.year, date.month, date.day)
+for paper_name, date in tqdm(product(paper_names, date_range), total=len(paper_names)*len(date_range)):
+    if paper_name == "daily mail":
+        url = "https://www.dailymail.co.uk/home/sitemaparchive/day_%04d%02d%02d.html" %(date.year, date.month, date.day)
+    elif paper_name == "daily star":
+        url = "https://www.dailystar.co.uk/sitearchive/%04d/%01d/%01d" %(date.year, date.month, date.day)
+    else:
+        raise ValueError("Paper not supported!")
 
     # Open page
     r = requests.get(url)
     if r.status_code != 200:
         continue
     soup = BeautifulSoup(r.text, 'html.parser')
-    titles = soup.find("ul", {"class": "archive-articles debate link-box"}).find_all("a")
+    if paper_name == 'daily mail':
+        titles = soup.find("ul", {"class": "archive-articles debate link-box"}).find_all("a")
+    elif paper_name == 'daily star':
+        titles = soup.find("ul", {"class": "section-list"}).find_all("a")
+    else:
+        titles = None
 
     # Store info only for titles containing fully capitalised words
     if len(titles) != 0:
@@ -36,12 +52,13 @@ for date in tqdm(pd.date_range(start_date, end_date)):
                 lowercase_text.append(title.lower())
                 one_hot.append(np.nonzero(capitalised_vector)[0])
                 pub_date.append(date)
+                paper.append(paper_name)
 
 # Turn into dframe and save
 print("Saving to CSV")
 dframe = pd.DataFrame(
-    {"headline": lowercase_text, "capitalised_index": one_hot,
-     "pub_date": pub_date})
+{"paper": paper, "headline": lowercase_text, "capitalised_index": one_hot,
+ "pub_date": pub_date})
 
 if os.path.isfile(database_name):
     dframe.to_csv(database_name, mode='a', header=False, index=False)
